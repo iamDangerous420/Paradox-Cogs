bd7cb579da47bcac90fb7e29975254cc4d28d62a
iamDangerous420 <aynfrncs@gmail.com>
2016-12-08 06:46:50 -0500

    Create info.py

---
+++ spam/info.py
@@ -0,0 +1,9 @@
+
+{
+    "AUTHOR" : "Dangerous",
+    "SHORT" : "Home made spam cog",
+    "DESCRIPTION" : "Selfmade spam Cog",
+    "DISABLED" : false,
+    "NAME" : "Spam",
+    "INSTALL_MSG" : "Thanks for installing my spam cog use at your own risk!"
+}
