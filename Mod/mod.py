86e0d275a43e3c36ca2724800b7872916e33f2e8
iamDangerous420 <aynfrncs@gmail.com>
2016-12-08 22:49:36 -0500

    new shit

--- Mod/mod.py
+++ Mod/mod.py
@@ -47,6 +47,8 @@ class Mod:
         self.ignore_list = dataIO.load_json("data/mod/ignorelist.json")
         self.filter = dataIO.load_json("data/mod/filter.json")
         self.past_names = dataIO.load_json("data/mod/past_names.json")
+        self._settings = dataIO.load_json('data/admin/settings.json')
+        self._settable_roles = self._settings.get("ROLES", {})
         self.past_nicknames = dataIO.load_json("data/mod/past_nicknames.json")
         settings = dataIO.load_json("data/mod/settings.json")
         self.settings = defaultdict(lambda: default_settings.copy(), settings)
@@ -56,6 +58,184 @@ class Mod:
         self._tmp_banned_cache = []
         perms_cache = dataIO.load_json("data/mod/perms_cache.json")
         self._perms_cache = defaultdict(dict, perms_cache)
+
+    def _get_selfrole_names(self, server):
+        if server.id not in self._settable_roles:
+            return None
+        else:
+            return self._settable_roles[server.id]
+
+    def _role_from_string(self, server, rolename, roles=None):
+        if roles is None:
+            roles = server.roles
+        role = discord.utils.find(lambda r: r.name.lower() == rolename.lower(),
+                                  roles)
+        try:
+            log.debug("Role {} found from rolename {}".format(
+                role.name, rolename))
+        except:
+            log.debug("Role not found for rolename {}".format(rolename))
+        return role
+
+    def _save_settings(self):
+        dataIO.save_json('data/admin/settings.json', self._settings)
+
+    def _set_selfroles(self, server, rolelist):
+        self._settable_roles[server.id] = rolelist
+        self._settings["ROLES"] = self._settable_roles
+        self._save_settings()
+    @commands.command(no_pm=True, pass_context=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    async def addrole(self, ctx, rolename, user: discord.Member=None):
+        """Adds a role to a user, defaults to author
+        Role name must be in quotes if there are spaces."""
+        author = ctx.message.author
+        channel = ctx.message.channel
+        server = ctx.message.server
+
+        if user is None:
+            user = author
+
+        role = self._role_from_string(server, rolename)
+
+        if role is None:
+            await self.bot.say('That role cannot be found.')
+            return
+
+        if not channel.permissions_for(server.me).manage_roles:
+            await self.bot.say('I don\'t have manage_roles.')
+            return
+
+        await self.bot.add_roles(user, role)
+        await self.bot.say('Added role {} to {}'.format(role.name, user.name))
+
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.mod_or_permissions(manage_roles=True)
+    async def createrole(self, context, *role_name):
+        """Create a role using the bot The role will be listed at the bottom of role list."""
+        server = context.message.server
+        name = ' '.join(role_name)
+        await self.bot.create_role(server, name= '{}'.format(name))
+        message = "I've **Succesfully** created the role `{}` :thumbsup:".format(name)
+        await self.bot.say(message)
+
+    @commands.group(pass_context=True, no_pm=True)
+    async def adminset(self, ctx):
+        """Manage Admin settings"""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @adminset.command(pass_context=True, name="selfroles")
+    @checks.admin_or_permissions(manage_roles=True)
+    async def adminset_selfroles(self, ctx, *, rolelist=None):
+        """Set which roles users can set themselves.
+        COMMA SEPARATED LIST (e.g. Admin,Staff,Mod)"""
+        server = ctx.message.server
+        if rolelist is None:
+            await self.bot.say("selfrole list cleared.")
+            self._set_selfroles(server, [])
+            return
+        unparsed_roles = list(map(lambda r: r.strip(), rolelist.split(',')))
+        parsed_roles = list(map(lambda r: self._role_from_string(server, r),
+                                unparsed_roles))
+        if len(unparsed_roles) != len(parsed_roles):
+            not_found = set(unparsed_roles) - {r.name for r in parsed_roles}
+            await self.bot.say(
+                "These roles were not found: {}\n\nPlease"
+                " try again.".format(not_found))
+        parsed_role_set = list({r.name for r in parsed_roles})
+        self._set_selfroles(server, parsed_role_set)
+        await self.bot.say(
+            "Self roles successfully set to: {}".format(parsed_role_set))
+
+    @commands.command(no_pm=True, pass_context=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    async def removerole(self, ctx, rolename, user: discord.Member=None):
+        """Removes a role from user, defaults to author
+        Role name must be in quotes if there are spaces."""
+        server = ctx.message.server
+        author = ctx.message.author
+
+        role = self._role_from_string(server, rolename)
+        if role is None:
+            await self.bot.say("Role not found.")
+            return
+
+        if user is None:
+            user = author
+
+        if role in user.roles:
+            try:
+                await self.bot.remove_roles(user, role)
+                await self.bot.say("Role successfully removed.")
+            except discord.Forbidden:
+                await self.bot.say("I don't have permissions to manage roles!")
+        else:
+            await self.bot.say("User does not have that role.")
+
+    @commands.group(no_pm=True, pass_context=True, invoke_without_command=True)
+    async def selfrole(self, ctx, *, rolename):
+        """Allows users to set their own role.
+        Configurable using `adminset`"""
+        server = ctx.message.server
+        author = ctx.message.author
+        role_names = self._get_selfrole_names(server)
+        if role_names is None:
+            await self.bot.say("I have no user settable roles for this"
+                               " server.")
+            return
+
+        roles = list(map(lambda r: self._role_from_string(server, r),
+                         role_names))
+
+        role_to_add = self._role_from_string(server, rolename, roles=roles)
+
+        try:
+            await self.bot.add_roles(author, role_to_add)
+        except discord.errors.Forbidden:
+            log.debug("{} just tried to add a role but I was forbidden".format(
+                author.name))
+            await self.bot.say("I don't have permissions to do that.")
+        except AttributeError:  # role_to_add is NoneType
+            log.debug("{} not found as settable on {}".format(rolename,
+                                                              server.id))
+            await self.bot.say("That role isn't user settable.")
+        else:
+            log.debug("Role {} added to {} on {}".format(rolename, author.name,
+                                                         server.id))
+            await self.bot.say("Role added.")
+
+    @selfrole.command(no_pm=True, pass_context=True, name="remove")
+    async def selfrole_remove(self, ctx, *, rolename):
+        """Allows users to remove their own roles
+        Configurable using `adminset`"""
+        server = ctx.message.server
+        author = ctx.message.author
+        role_names = self._get_selfrole_names(server)
+        if role_names is None:
+            await self.bot.say("I have no user settable roles for this"
+                               " server.")
+            return
+
+        roles = list(map(lambda r: self._role_from_string(server, r),
+                         role_names))
+        role_to_remove = self._role_from_string(server, rolename, roles=roles)
+
+        try:
+            await self.bot.remove_roles(author, role_to_remove)
+        except discord.errors.Forbidden:
+            log.debug("{} just tried to remove a role but I was"
+                      " forbidden".format(author.name))
+            await self.bot.say("I don't have permissions to do that.")
+        except AttributeError:  # role_to_remove is NoneType
+            log.debug("{} not found as removeable on {}".format(rolename,
+                                                                server.id))
+            await self.bot.say("That role isn't user removeable.")
+        else:
+            log.debug("Role {} removed from {} on {}".format(rolename,
+                                                             author.name,
+                                                             server.id))
+            await self.bot.say("Role removed.")
 
     @commands.group(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(administrator=True)
@@ -1311,6 +1491,14 @@ def check_files():
         if not os.path.isfile("data/mod/{}".format(filename)):
             print("Creating empty {}".format(filename))
             dataIO.save_json("data/mod/{}".format(filename), value)
+
+    if not os.path.exists('data/admin/settings.json'):
+        try:
+            os.mkdir('data/admin')
+        except FileExistsError:
+            pass
+        else:
+            dataIO.save_json('data/admin/settings.json', {})
 
 
 
