be6bb32ab972667a5a8af698bf2f00a21a75e256
iamDangerous420 <aynfrncs@gmail.com>
2016-12-17 20:44:32 -0500

    Add files via upload

--- general/general.py
+++ general/general.py
@@ -14,7 +14,9 @@ from .utils import checks
 import asyncio
 from cogs.utils.dataIO import dataIO
 import io, os
-from cogs.utils.dataIO import dataIO
+from .utils.dataIO import fileIO
+import logging
+
 
 settings = {"POLL_DURATION" : 60}
 
@@ -27,6 +29,8 @@ class General:
         self.bot = bot
         self.data = dataIO.load_json(JSON)
         self.stopwatches = {}
+        self.reminders = fileIO("data/remindme/reminders.json", "load")
+        self.units = {"minute" : 60, "hour" : 3600, "day" : 86400, "week": 604800, "month": 2592000}
         self.settings = 'data/youtube/settings.json'
         self.youtube_regex = (
           r'(https?://)?(www\.)?'
@@ -122,6 +126,25 @@ class General:
                         em = discord.Embed(color=discord.Color.purple())
                         em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
                     await self.bot.send_message(message.channel, embed=em)
+
+    async def check_reminders(self):
+        while self is self.bot.get_cog("RemindMe"):
+            to_remove = []
+            for reminder in self.reminders:
+                if reminder["FUTURE"] <= int(time.time()):
+                    try:
+                        await self.bot.send_message(discord.User(id=reminder["ID"]), "**Hey!!** {} You've asked me to remind you this \n{}".format(user.name, reminder["TEXT"]))
+                    except (discord.errors.Forbidden, discord.errors.NotFound):
+                        to_remove.append(reminder)
+                    except discord.errors.HTTPException:
+                        pass
+                    else:
+                        to_remove.append(reminder)
+            for reminder in to_remove:
+                self.reminders.remove(reminder)
+            if to_remove:
+                fileIO("data/remindme/reminders.json", "save", self.reminders)
+            await asyncio.sleep(5)
 
     @commands.command(pass_context=True, name="away")
     async def _away(self, context, *message: str):
@@ -460,6 +483,71 @@ class General:
         pingms = await self.bot.say("pinging server...")
         ping = time.time() - pingtime
         await self.bot.edit_message(pingms, "It took **%.01f** secs" % (ping) + " to ping.")
+
+    @commands.command(pass_context=True)
+    async def remind(self, ctx,  quantity : int, time_unit : str, *, text : str):
+        """Sends you <text> when the time has elapsed
+        Units: minutes, hours, days, weeks, month
+        Example:
+        [p]remind 3 days Kill myself
+        Cog by 26."""
+        time_unit = time_unit.lower()
+        author = ctx.message.author
+        s = ""
+        if time_unit.endswith("s"):
+            time_unit = time_unit[:-1]
+            s = "s"
+        if not time_unit in self.units:
+            await self.bot.say(":noo_good: Time units are **As Follows**:\n minute, hour, day, week, month")
+            return
+        if quantity < 1:
+            await self.bot.say(":bangbang: Number cannot be negative or lower.:x:")
+            return
+        if len(text) > 1960:
+            await self.bot.say("https://goo.gl/Me042H That text is too long Boi.")
+            return
+        seconds = self.units[time_unit] * quantity
+        future = int(time.time()+seconds)
+        self.reminders.append({"ID" : author.id, "FUTURE" : future, "TEXT" : text})
+        logger.info("{} ({}) set a reminder.".format(author.name, author.id))
+        await self.bot.say(":thumbsup: **Gotcha !!** Ima remind you that in ***{} {}. :smile:***".format(str(quantity), time_unit + s))
+        fileIO("data/remindme/reminders.json", "save", self.reminders)
+
+    @commands.command(pass_context=True)
+    async def forget(self, ctx):
+        """Removes all your upcoming notifications"""
+        author = ctx.message.author
+        to_remove = []
+        for reminder in self.reminders:
+            if reminder["ID"] == author.id:
+                to_remove.append(reminder)
+
+        if not to_remove == []:
+            for reminder in to_remove:
+                self.reminders.remove(reminder)
+            fileIO("data/remindme/reminders.json", "save", self.reminders)
+            await self.bot.say("**Notifications Removed** :thumbsup:")
+        else:
+            await self.bot.say(":no_good: You have **No** Notifications :thinking:")
+
+    async def check_reminders(self):
+        while self is self.bot.get_cog("RemindMe"):
+            to_remove = []
+            for reminder in self.reminders:
+                if reminder["FUTURE"] <= int(time.time()):
+                    try:
+                        await self.bot.send_message(discord.User(id=reminder["ID"]), "**Hey!!** {} You've asked me to remind you this \n{}".format(user.name, reminder["TEXT"]))
+                    except (discord.errors.Forbidden, discord.errors.NotFound):
+                        to_remove.append(reminder)
+                    except discord.errors.HTTPException:
+                        pass
+                    else:
+                        to_remove.append(reminder)
+            for reminder in to_remove:
+                self.reminders.remove(reminder)
+            if to_remove:
+                fileIO("data/remindme/reminders.json", "save", self.reminders)
+            await asyncio.sleep(5)
 
     @commands.command(pass_context=True)
     async def roll(self, ctx, number : int = 100):
@@ -1004,6 +1092,25 @@ class NewPoll():
         await self.client.send_message(self.channel, msg)
         self.poll_sessions.remove(self)
 
+    async def check_reminders(self):
+        while self is self.bot.get_cog("RemindMe"):
+            to_remove = []
+            for reminder in self.reminders:
+                if reminder["FUTURE"] <= int(time.time()):
+                    try:
+                        await self.bot.send_message(discord.User(id=reminder["ID"]), "**Hey!!** {} You've asked me to remind you this \n{}".format(user.name, reminder["TEXT"]))
+                    except (discord.errors.Forbidden, discord.errors.NotFound):
+                        to_remove.append(reminder)
+                    except discord.errors.HTTPException:
+                        pass
+                    else:
+                        to_remove.append(reminder)
+            for reminder in to_remove:
+                self.reminders.remove(reminder)
+            if to_remove:
+                fileIO("data/remindme/reminders.json", "save", self.reminders)
+            await asyncio.sleep(5)
+
     def checkAnswer(self, message):
         try:
             i = int(message.content)
@@ -1037,6 +1144,11 @@ def check_file():
         print("Creating default settings.json...")
         dataIO.save_json(f, data)
 
+    f = "data/remindme/reminders.json"
+    if not fileIO(f, "check"):
+        print("Creating empty reminders.json...")
+        fileIO(f, "save", [])
+
 
 
 
@@ -1061,10 +1173,23 @@ def check_folder():
         print("Creating data/youtube folder...")
         os.makedirs("data/youtube")
 
+    if not os.path.exists("data/remindme"):
+        print("Creating data/remindme folder...")
+        os.makedirs("data/remindme")
+
 def setup(bot):
+    global logger
     check_folder()
     check_file()
     n = General(bot)
+    logger = logging.getLogger("remindme")
+    if logger.level == 0: # Prevents the logger from being loaded again in case of module reload
+        logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename='data/remindme/reminders.log', encoding='utf-8', mode='a')
+        handler.setFormatter(logging.Formatter('%(asctime)s %(message)s', datefmt="[%d/%m/%Y %H:%M]"))
+        logger.addHandler(handler)
     bot.add_listener(n.check_poll_votes, "on_message")
     bot.add_listener(n.listener, 'on_message')
+    loop = asyncio.get_event_loop()
+    loop.create_task(n.check_reminders())
     bot.add_cog(n)
