e7941542f18299d7edf192453bb653705d71e03e
iamDangerous420 <aynfrncs@gmail.com>
2016-12-18 20:58:38 -0500

    Add files via upload

--- mod/mod.py
+++ mod/mod.py
@@ -319,20 +319,23 @@ class Mod:
             await self.bot.say("**SelfRole List CLEARED** :thumbsup:")
             self._set_selfroles(server, [])
             return
-	@adminset.command(pass_context=True, name="selfroles")
+    @adminset.command(pass_context=True, name="selfroles")
     @checks.admin_or_permissions(manage_roles=True)
     async def adminset_selfroles(self, ctx, *, rolelist=None):
         """Set which roles users can set themselves.
         COMMA SEPARATED LIST (e.g. Admin,Staff,Mod)"""
         server = ctx.message.server
+        if rolelist is None:
+            await self.bot.say("**RoleList Is none** If you're trying to clear the list please do {}adminset selfrolesclear".format(ctx.prefix))
+            return
         unparsed_roles = list(map(lambda r: r.strip(), rolelist.split(',')))
         parsed_roles = list(map(lambda r: self._role_from_string(server, r),
                                 unparsed_roles))
         if len(unparsed_roles) != len(parsed_roles):
             not_found = set(unparsed_roles) - {r.name for r in parsed_roles}
             await self.bot.say(
-                "These roles were not found: {}\n\nPlease"
-                " try again.".format(not_found))
+                ":x: These roles were **not found:** `{}`\n\nPlease"
+                " try again. :frowning:".format(not_found))
         parsed_role_set = list({r.name for r in parsed_roles})
         self._set_selfroles(server, parsed_role_set)
         await self.bot.say(
