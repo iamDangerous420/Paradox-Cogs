705d545e4a9ace95a53fe09871f60eb69e172b07
iamDangerous420 <aynfrncs@gmail.com>
2016-12-22 09:43:11 -0500

    Add files via upload

--- mod/mod.py
+++ mod/mod.py
@@ -753,43 +753,33 @@ class Mod:
             counter = counter + 1
             if counter == 1:
                 await self.bot.say("**Feeling foken sorry for {} they got spammed alright**".format(user.name))
-    @commands.command(hidden=True)
+    @commands.command(pass_context=True)
     @checks.mod_or_permissions(manage_messages=True)
-    async def gspam(self, ctx, user : discord.Member, number : int=30):
-        """Ghost spam same as normal spam but they will never know it was you eyes emoji."""
+    async def gspam(self, ctx, user : discord.Member, spamtext, number : int=30):
+        """Ghost spam same as normal spam but they will never know it was you eyes emoji. default 30"""
         if user.id == "187570149207834624" or user.id == "217256996309565441":
             await self.bot.say("Oh **HELLL NAH** I aint spamming that dude **HIS NAME IS** ***DANGEROUS*** **WHAT DO YOU NOT UNDERSTAND FROM THAT**")
             return
         if number> 199:
                 await self.bot.reply("Cannot spam more than 200 msgs lag purposes sorry !")
                 return
-        counter = 0
         while counter < number:
             await self.bot.send_message(user, "***You got spamed  punk (╯°□°）╯︵ ┻━┻!*** By ***Anonymous*** ** ¯\_(ツ)_/¯!**.")
-            counter = counter + 1
-            if counter == 1:
-                await self.bot.say("**spammed**".format(user.name))
+            await self.bot.say("**spammed**".format(user.name))
     @commands.command(pass_context=True)
     @checks.mod_or_permissions()
-    async def cspam(self, ctx, spamtext, number : int=None):
-        """Spams x times in the channel, default is 10."""
-        if number == None:
-            number = 10
-        counter = 0
+    async def cspam(self, ctx, spamtext, number : int=10):
+        """Spams the channel, default =10."""
         while counter < number:
             await self.bot.say("{}, sent by **{}**.".format(spamtext, ctx.message.author))
-            counter = counter + 1
     @commands.command(pass_context=True)
     @checks.mod_or_permissions()
-    async def gcspam(self, ctx, spamtext, number : int=None):
+    async def gcspam(self, ctx, spamtext, number : int=10):
         """Spams x times in the channel anonymously, default is 10."""
-        if number == None:      
-            number = 10
-        counter = 0
+
         await self.bot.delete_message(ctx.message)
         while counter < number:
             await self.bot.say("{} Sent By ***Anonymous***".format(spamtext))
-            counter = counter + 1  
     @commands.group(pass_context=True)
     @checks.mod_or_permissions(manage_messages=True)
     async def cleanup(self, ctx):
