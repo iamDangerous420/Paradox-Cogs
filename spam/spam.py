629ebada46446a0b0b74082c32bd9239a2f7acb7
iamDangerous420 <aynfrncs@gmail.com>
2017-01-02 21:02:20 -0500

    ERRORS D;

--- spam/spam.py
+++ spam/spam.py
@@ -3,6 +3,7 @@ from .utils import checks
 from __main__ import settings
 from discord.ext import commands
 from discord.errors import DiscordException
+import asyncio
 
 class spam:
     """Spams."""
@@ -27,8 +28,9 @@ class spam:
         await self.bot.say("**Feeling foken sorry for {} they got spammed alright**".format(user.name))
     @commands.command(pass_context=True)
     @checks.mod_or_permissions(manage_messages=True)
-    async def gspam(self, ctx, user : discord.Member, spamtext, number : int=30):
-        """Ghost spam same as normal spam but they will never know it was you eyes emoji. default 30"""
+    async def tspam(self, ctx, user : discord.Member, spamtext, number : int=30):
+        """same as normal spam but with text default 30
+        If it doesn't work first try probs cause yo message to long so use ==> "" """
         if user.id == "187570149207834624" or user.id == "217256996309565441":
             await self.bot.say("Oh **HELLL NAH** I aint spamming that dude **HIS NAME IS** ***DANGEROUS*** **WHAT DO YOU NOT UNDERSTAND FROM THAT**")
             return
@@ -37,10 +39,9 @@ class spam:
                 return
         counter = 0
         while counter < number:
-            await self.bot.send_message(user, "***You got spamed (╯°□°）╯︵ ┻━┻! ***MESSAGE IS:*** ***```{}```*** *** By ***Anonymous*** ** ¯\_(ツ)_/¯!**.\n".format(spamtext))
-            await self.bot.delete_message(ctx.message)
+            await self.bot.send_message(user, "***You got spamed (╯°□°）╯︵ ┻━┻! ***MESSAGE IS:*** ***```\n{}\n```*** *** From ***{}*** ** ¯\_(ツ)_/¯!**.\n".format(spamtext, user.name))
             counter = counter + 1
-        await self.bot.say("**spammed {}**".format(user.name))
+        await self.bot.say("**Feeling foken sorry for {} they got spammed alright**".format(user.name))
     @commands.command(pass_context=True)
     @checks.mod_or_permissions()
     async def cspam(self, ctx, spamtext, number : int=10):
