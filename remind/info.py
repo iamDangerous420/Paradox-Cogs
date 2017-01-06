02080d26d12b55442d30ce930bf3171a0dfaca18
iamDangerous420 <aynfrncs@gmail.com>
2017-01-06 09:04:17 -0500

    Create info.py

---
+++ remind/info.py
@@ -0,0 +1,8 @@
+{
+    "AUTHOR" : "Dangerous",
+    "SHORT" : "Homeedited reminder cog",
+    "DESCRIPTION" : "SelfEdited reminder Cog from Red.",
+    "DISABLED" : false,
+    "NAME" : "reminder",
+    "INSTALL_MSG" : "***```Thanks for installing my edited reminder cog !```***"
+}
