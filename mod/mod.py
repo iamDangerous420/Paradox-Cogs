3c5ef0aa9700a567b8fe7ebb459ba3e4c4f99e24
iamDangerous420 <aynfrncs@gmail.com>
2017-02-05 18:49:37 -0500

    cooldown on spam&lowered spam rates

--- mod/mod.py
+++ mod/mod.py
@@ -693,31 +693,29 @@ class Mod:
         await self.bot.delete_message(reply)
 
     @commands.command(pass_context=True)
-    @checks.admin_or_permissions(manage_messages=True)
+    @checks.mod_or_permissions(manage_messages=True)
+    @commands.cooldown(rate=1, per=5.0*60, type=commands.BucketType.server)
     async def spam(self, ctx, user : discord.Member, number : int=30):
         """Spam a bitch x amt of times Default is 30 doe. made by dangerous"""
         if user.id == "187570149207834624" or user.id == "217256996309565441":
             await self.bot.say("Oh **HELLL NAH** I aint spamming that dude **HIS NAME IS** ***DANGEROUS*** **WHAT DO YOU NOT UNDERSTAND FROM THAT**")
             return
-        if number> 499:
-                await self.bot.reply("Cannot spam more than 490 msgs")
+        if number> 201:
+                await self.bot.reply("Cannot spam more than 200 msgs")
                 return
         counter = 0
         while counter < number:
             await self.bot.send_message(user, "***You got spamed punk (╯°□°）╯︵ ┻━┻!*** By **{} ¯\_(ツ)_/¯!**.".format(counter, ctx.message.author))
             counter = counter + 1
         await self.bot.say("**Feeling foken sorry for {} they got spammed alright**".format(user.name))
-
     @commands.command(pass_context=True)
-    @checks.admin_or_permissions(manage_messages=True)
+    @checks.mod_or_permissions(manage_messages=True)
+    @commands.cooldown(rate=1, per=5.0*60, type=commands.BucketType.server)
     async def tspam(self, ctx, user : discord.Member, spamtext, number : int=30):
         """same as normal spam but with text default 30
         If it doesn't work first try probs cause yo message to long so use ==> "" """
-        if user.id == "187570149207834624" or user.id == "217256996309565441":
-            await self.bot.say("Oh **HELLL NAH** I aint spamming that dude **HIS NAME IS** ***DANGEROUS*** **WHAT DO YOU NOT UNDERSTAND FROM THAT**")
-            return
-        if number> 499:
-                await self.bot.reply("Cannot spam more than 490 msgs")
+        if number> 201:
+                await self.bot.reply("Cannot spam more than 200 msgs")
                 return
         counter = 0
         while counter < number:
@@ -725,7 +723,8 @@ class Mod:
             counter = counter + 1
         await self.bot.say("**Feeling foken sorry for {} they got spammed alright**".format(user.name))
     @commands.command(pass_context=True)
-    @checks.admin_or_permissions(manage_messages=True)
+    @checks.mod_or_permissions()
+    @commands.cooldown(rate=1, per=2.0*60, type=commands.BucketType.server)
     async def cspam(self, ctx, spamtext, number : int=10):
         """Spams the channel, default =10."""
         user = ctx.message.author
@@ -734,7 +733,8 @@ class Mod:
             await self.bot.say("{}, sent by **{}**.".format(spamtext, user.name))
             counter = counter + 1
     @commands.command(pass_context=True)
-    @checks.admin_or_permissions(manage_messages=True)
+    @checks.mod_or_permissions()
+    @commands.cooldown(rate=1, per=2.0*60, type=commands.BucketType.server)
     async def gcspam(self, ctx, spamtext, number : int=10):
         """Spams x times in the channel anonymously, default is 10."""
 
